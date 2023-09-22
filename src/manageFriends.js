d7cb8b4e9d8880bf6b5467ce473b28e226742ace
Luna Seifert <thepuddingdeity@gmail.com>
2023-09-22 12:14:24 -0500

    commit tree e2a4eb15ddf1132ced150ff8b6402efd0214b1fc

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,15 @@
-export function manageFriends(state, action) {
-  // your code here
+export function manageFriends(state, action){
+  const r = Object.assign({}, state)
+
+  switch (action.type){
+    case "friends/add":
+      r.friends.push(action.payload)
+      return r
+    case "friends/remove":
+      r.friends = r.friends.filter(friend => friend.id !== action.payload)
+      return r
+    default:
+      return state
+  }
+
 }
